5d7126798a928a277ada2f5f9e69565c34b231f2
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-07-31 14:19:39 +0200

    Moved the variables class to misc, added functions class

---
+++ ScuffClient/Misc/Functions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using VRC;
+
+namespace ScuffClient.Misc
+{
+    public class Functions
+    {
+        public static int GetPhotonId(Player p) => p.GetInstigatorId().GetValueOrDefault();
+
+        //generate new hwid to avoid hwid bans
+        public static string NewDeviceID()
+        {
+            return KeyedHashAlgorithm.Create().ComputeHash(Encoding.UTF8.GetBytes(string.Format("{0}D-{1}{2}-{3}{4}-{5}{6}-72-5A", new object[]
+            {
+                new Random().Next(0, 9),
+                new Random().Next(0, 9),
+                new Random().Next(0, 9),
+                new Random().Next(0, 9),
+                new Random().Next(0, 9),
+                new Random().Next(0, 9),
+                new Random().Next(0, 9)
+            }))).Select(delegate (byte x)
+            {
+                byte b = x;
+                return b.ToString("x2");
+            }).Aggregate((string x, string y) => x + y);
+        }
+    }
+}
---
+++ ScuffClient/Misc/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using VRC;
+
+namespace ScuffClient.Misc
+{
+    public class Variables
+    {
+        public static bool antiPortal;
+    }
+}
