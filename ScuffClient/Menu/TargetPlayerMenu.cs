5f24c71fc7d78e253793e33731a7734d2070fca2
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-08-04 20:29:34 +0200

    Cleaned up code by making each specific button its own method

--- ScuffClient/Menu/TargetPlayerMenu.cs
+++ ScuffClient/Menu/TargetPlayerMenu.cs
@@ -10,7 +10,7 @@ using ScuffClient.Misc;
 
 namespace ScuffClient.Menu
 {
-    public class TargetPlayerMenu
+    public class TargetPlayerMenu : MonoBehaviour
     {
         private MethodInfo m_QuickMenuInstance;
         private FieldInfo f_SelectedUser;
@@ -20,6 +20,8 @@ namespace ScuffClient.Menu
         private APIUser selectedAPIUser;
         private GameObject portalButton;
         private GameObject logoutButton;
+        private GameObject teleportButton;
+        private GameObject cloneButton;
 
         public void Update()
         {
@@ -31,38 +33,10 @@ namespace ScuffClient.Menu
                 {
                     selectedPlayer = PlayerManager.GetPlayer(selectedAPIUser.id);
                     selectedVRCPlayer = selectedPlayer.vrcPlayer;
-
-                    #region PortalButton
-                    portalButton = GameObject.Find("UserInteractMenu").transform.Find("BanButton").gameObject;
-                    ColorBlock colors = portalButton.GetComponent<Button>().colors;
-                    colors.normalColor = new Color(0f, 1f, 0f, 0.5f);
-                    colors.highlightedColor = new Color(0f, 1f, 0f, 1f);
-                    colors.pressedColor = new Color(0f, 1f, 0f);
-                    portalButton.SetActive(true);
-                    portalButton.GetComponentInChildren<Text>().text = "Portal";
-                    portalButton.GetComponent<Button>().colors = colors;
-                    portalButton.GetComponent<Button>().onClick.RemoveAllListeners();
-                    portalButton.GetComponent<Button>().onClick.AddListener(delegate ()
-                    {
-                        Exploits.DropPortal(selectedPlayer);
-                    });
-                    #endregion
-
-                    #region LogoutButton
-                    logoutButton = GameObject.Find("UserInterActMenu").transform.Find("BanButton").gameObject;
-                    ColorBlock colors2 = portalButton.GetComponent<Button>().colors;
-                    colors.normalColor = new Color(1f, 0f, 0.5f, 0.5f);
-                    colors.highlightedColor = new Color(1f, 0f, 0.5f, 1f);
-                    colors.pressedColor = new Color(1f, 0f, 1f);
-                    portalButton.SetActive(true);
-                    portalButton.GetComponentInChildren<Text>().text = "Logout";
-                    portalButton.GetComponent<Button>().colors = colors;
-                    portalButton.GetComponent<Button>().onClick.RemoveAllListeners();
-                    portalButton.GetComponent<Button>().onClick.AddListener(delegate ()
-                    {
-                        Exploits.TriggerIndexOutOfRange(selectedPlayer);
-                    });
-                    #endregion
+                    LoadPortalButton();
+                    LoadCloneButton();
+                    LoadLogoutButton();
+                    LoadTeleportButton();
                 }
             }
             catch { }
@@ -72,6 +46,86 @@ namespace ScuffClient.Menu
         {
             m_QuickMenuInstance = typeof(QuickMenu).GetProperties().First((PropertyInfo p) => p.PropertyType == typeof(QuickMenu)).GetGetMethod();
             f_SelectedUser = typeof(QuickMenu).GetFields(BindingFlags.Instance | BindingFlags.NonPublic).First((FieldInfo f) => f.FieldType == typeof(APIUser));
+        }
+        
+        public void LoadLogoutButton()
+        {
+            logoutButton = GameObject.Find("UserInteractMenu").transform.Find("ForceLogoutButton").gameObject;
+            ColorBlock logoutColor = portalButton.GetComponent<Button>().colors;
+            logoutColor.normalColor = new Color(1f, 0f, 1f, 0.5f);
+            logoutColor.highlightedColor = new Color(1f, 0f, 1f, 1f);
+            logoutColor.pressedColor = new Color(1f, 0f, 1f);
+            logoutButton.SetActive(true);
+            logoutButton.GetComponentInChildren<Text>().text = "Logout";
+            logoutButton.GetComponent<Button>().colors = logoutColor;
+            logoutButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            logoutButton.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                Exploits.TriggerIndexOutOfRange(selectedPlayer);
+            });
+        }
+        
+        public void LoadPortalButton()
+        {
+            portalButton = GameObject.Find("UserInteractMenu").transform.Find("BanButton").gameObject;
+            ColorBlock portalColor = portalButton.GetComponent<Button>().colors;
+            portalColor.normalColor = new Color(0f, 1f, 0f, 0.5f);
+            portalColor.highlightedColor = new Color(0f, 1f, 0f, 1f);
+            portalColor.pressedColor = new Color(0f, 1f, 0f);
+            portalButton.SetActive(true);
+            portalButton.GetComponentInChildren<Text>().text = "Portal";
+            portalButton.GetComponent<Button>().colors = portalColor;
+            portalButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            portalButton.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                Exploits.DropPortal(selectedPlayer);
+            });
+        }
+
+        public void LoadTeleportButton()
+        {
+            teleportButton = GameObject.Find("UserInteractMenu").transform.Find("KickButton").gameObject;
+            ColorBlock teleportColor = teleportButton.GetComponent<Button>().colors;
+            teleportColor.normalColor = new Color(0f, 1f, 1f, 0.5f);
+            teleportColor.highlightedColor = new Color(0f, 1f, 1f, 1f);
+            teleportColor.pressedColor = new Color(0f, 1f, 1f);
+            teleportButton.SetActive(true);
+            teleportButton.GetComponentInChildren<Text>().text = "Teleport";
+            teleportButton.GetComponent<Button>().colors = teleportColor;
+            teleportButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            teleportButton.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                Exploits.Teleport(selectedPlayer);
+            });
+        }
+
+        public void LoadCloneButton()
+        {
+            if (PlayerReflections.GetApiAvatar(selectedPlayer).releaseStatus == "public")
+            {
+                cloneButton = GameObject.Find("UserInteractMenu").transform.Find("WarnButton").gameObject;
+                ColorBlock publicColor = portalButton.GetComponent<Button>().colors;
+                publicColor.normalColor = new Color(0f, 1f, 0f, 0.5f);
+                publicColor.highlightedColor = new Color(0f, 1f, 0f, 1f);
+                publicColor.pressedColor = new Color(0f, 1f, 1f);
+                cloneButton.SetActive(true);
+                cloneButton.GetComponentInChildren<Text>().text = "Yoink";
+                cloneButton.GetComponent<Button>().colors = publicColor;
+                cloneButton.GetComponent<Button>().onClick.RemoveAllListeners();
+                cloneButton.GetComponent<Button>().onClick.AddListener(delegate ()
+                {
+                    Exploits.LoadAvatarFromId(PlayerReflections.GetApiAvatar(selectedPlayer).id);
+                });
+                cloneButton.GetComponent<Button>().interactable = true;
+            }
+            else
+            {
+                cloneButton = GameObject.Find("UserInteractMenu").transform.Find("WarnButton").gameObject;
+                cloneButton.SetActive(true);
+                cloneButton.GetComponentInChildren<Text>().text = "PRIVATE";
+                cloneButton.GetComponent<Button>().onClick.RemoveAllListeners();
+                cloneButton.GetComponent<Button>().interactable = false;
+            }
         }
     }
 }
