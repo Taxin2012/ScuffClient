d298cf3212b6d0508a5f6e5f033f2f6af7e24ae9
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-08-13 22:33:49 +0200

    implemented flying

--- ScuffClient/Misc/Cheats.cs
+++ ScuffClient/Misc/Cheats.cs
@@ -9,17 +9,17 @@ namespace ScuffClient.Misc
         private bool isNoclip;
         private bool isSpeedHacking;
         private LocomotionInputController controller;
-        private VRCMotionState motion;
         private Vector3 originalGravity;
 
         public void Update()
         {
             try
             {
-                if(VRCPlayer.Instance != null && (controller == null || motion == null))
+                if (Event.current.shift && Input.GetKeyDown(KeyCode.Alpha1))
+                    ToggleFly();
+                if(VRCPlayer.Instance != null && controller == null)
                 {
                     controller = VRCPlayer.Instance.GetComponent<LocomotionInputController>();
-                    motion = VRCPlayer.Instance.GetComponent<VRCMotionState>();
                 }
                 Flying();
                 NoClip();
@@ -31,6 +31,15 @@ namespace ScuffClient.Misc
         public void ToggleFly()
         {
             isFlying = !isFlying;
+
+            if (isFlying)
+            {
+                originalGravity = Physics.gravity;
+                Physics.gravity = Vector3.zero;
+            }
+            else
+                Physics.gravity = originalGravity;
+
         }
         public void ToggleNoClip()
         {
@@ -43,25 +52,15 @@ namespace ScuffClient.Misc
         #endregion
         private void Flying()
         {
-            if(isFlying)
+            if (isFlying)
             {
-                originalGravity = Physics.gravity;
-                Physics.gravity = Vector3.zero;
-
                 Vector3 pos = VRCPlayer.Instance.transform.position;
 
-                if(Input.GetKey(KeyCode.Q))
+                if (Input.GetKey(KeyCode.Q))
                     VRCPlayer.Instance.transform.position = new Vector3(pos.x, pos.y - (12 * Variables.speedHackValue) * Time.deltaTime, pos.z);
-                
-                if(Input.GetKey(KeyCode.E))
+
+                if (Input.GetKey(KeyCode.E))
                     VRCPlayer.Instance.transform.position = new Vector3(pos.x, pos.y + (12 * Variables.speedHackValue) * Time.deltaTime, pos.z);
-            }
-            if(!isFlying)
-            {
-                Physics.gravity = originalGravity;
-                controller.strafeSpeed = 2f;
-                controller.walkSpeed = 2f;
-                controller.runSpeed = 4f;
             }
         }
         private void NoClip()
