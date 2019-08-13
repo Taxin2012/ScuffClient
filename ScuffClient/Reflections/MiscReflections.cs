4249b03632d118de48142afb072a3b66fc98687b
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-08-13 23:05:37 +0200

    added getmode reflection for camera mode

--- ScuffClient/Reflections/MiscReflections.cs
+++ ScuffClient/Reflections/MiscReflections.cs
@@ -7,6 +7,7 @@ namespace ScuffClient.Reflections
     {
         #region MiscReflections
         private readonly static MethodInfo m_setCameraMode = typeof(UserCameraController).GetMethod("set_mode");
+        private readonly static MethodInfo m_getCameraMode = typeof(UserCameraController).GetMethod("get_mode");
         private readonly static MethodInfo m_FlowInstanceMethod = typeof(VRCFlowManager).GetMethod("get_instance");
         private readonly static MethodInfo m_QuickMenuInstance = typeof(QuickMenu).GetMethod("get_instance");
         private readonly static MethodInfo m_UiManagerInstanceMethod = typeof(VRCUiManager).GetProperties().FirstOrDefault(p => p.PropertyType == typeof(VRCUiManager))?.GetGetMethod();
@@ -14,6 +15,7 @@ namespace ScuffClient.Reflections
 
         public static VRCUiManager GetUiManagerInstance() => (VRCUiManager)m_UiManagerInstanceMethod.Invoke(null, null);
         public static void SetCameraMode(int mode) => m_setCameraMode.Invoke(UserCameraController.Instance, new object[] { (mode <= 2) ? mode : 1 });
+        public static int GetCameraMode() => (int)m_getCameraMode.Invoke(UserCameraController.Instance, null);
         public static QuickMenu GetQuickMenuInstance() => (QuickMenu)m_QuickMenuInstance.Invoke(null, null);
 
     }
