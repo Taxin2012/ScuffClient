7b2a7c57e58efc8fc7eb39b8c82026d6d1cc81ce
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-09-05 04:15:32 +0200

    defaulted anti-portal variable to false, added blockedColor, fov and steamid variables for config settings

--- ScuffClient/Misc/Variables.cs
+++ ScuffClient/Misc/Variables.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using VRC;
+using UnityEngine;
 
 namespace ScuffClient.Misc
 {
     public class Variables
     {
-        public static bool antiPortal;
+        public static bool antiPortal = false;
         public static float speedHackValue = 1f;
+        public static Color blockedColor;
+        public static float fieldOfView;
+        public static string steamId;
     }
 }
