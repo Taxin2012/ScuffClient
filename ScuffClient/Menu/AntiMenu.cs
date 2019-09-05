ae9fc0b3ccf0c2676c5c76d812c27cffa0d9d4eb
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-09-05 04:11:30 +0200

    Added menu and relevant buttons

--- ScuffClient/Menu/AntiMenu.cs
+++ ScuffClient/Menu/AntiMenu.cs
@@ -19,6 +19,9 @@ namespace ScuffClient.Menu
         private GameObject customMenuButton;
         private GameObject customMenu;
         private GameObject cameraMenu;
+
+        private GameObject antiPortalButton;
+        private GameObject flyButton;
         #endregion
 
         public void Update()
@@ -29,10 +32,15 @@ namespace ScuffClient.Menu
             if (IsGONull(customMenuButton))
                 CreateCustomMenuButton();
 
-            //if (IsGONull(customMenu))
-                //CustomMenu();
-            
+            if (IsGONull(customMenu))
+                CreateCustomMenu();
 
+            if (IsGONull(flyButton))
+                CreateFlyButton();
+
+            if (IsGONull(antiPortalButton))
+                CreateAntiPortalButton();
+            
             try
             {
                 menu = (QuickMenu)m_QuickMenuInstance.Invoke(null, null);
@@ -82,17 +90,68 @@ namespace ScuffClient.Menu
             colors.pressedColor = themeColor;
             customButton.colors = colors;
 
-            customButton.onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
-            customButton.onClick.RemoveAllListeners();
-            customButton.onClick.AddListener(delegate ()
+            customMenuButton.GetComponent<UiTooltip>().text = "Open ScuffClient custom menu";
+        }
+
+        private void CreateCustomMenu()
+        {
+            customMenu = menu.transform.Find("ModerationMenu").gameObject;
+            foreach (Component comp in customMenu.GetComponentsInChildren<Component>())
             {
-                Exploits.DropPortal();
+                if (comp.GetComponent<Button>() != null && comp.name.ToLower() != "backbutton")
+                {
+                    comp.gameObject.SetActive(false);
+                }
+
+                if (comp.GetComponent<Button>() != null && comp.name.ToLower() == "backbutton")
+                {
+                    comp.GetComponent<Button>().colors = Functions.SetThemeColor(comp.GetComponent<Button>().colors);
+                    comp.GetComponent<Button>().GetComponentInChildren<Text>().text = "return";
+                    comp.GetComponent<Button>().GetComponentInChildren<Text>().color = Color.white;
+                }
+            }
+        }
+
+        private void CreateAntiPortalButton()
+        {
+            GameObject original = customMenu.transform.Find("ClearRoomHub").gameObject;
+            antiPortalButton = GameObject.Instantiate(original, original.transform.position, default(Quaternion), customMenu.transform);
+            antiPortalButton.SetActive(true);
+            antiPortalButton.name = "AntiPortalButton";
+            antiPortalButton.GetComponent<UiTooltip>().text = "Toggle Anti-Portal On/Off";
+            antiPortalButton.GetComponentInChildren<Text>().text = !Variables.antiPortal ? "Enable\nPortals" : "Disable\nPortals";
+            antiPortalButton.GetComponentInChildren<Text>().color = !Variables.antiPortal ? Color.red : Color.green;
+            antiPortalButton.GetComponent<Button>().colors = Functions.SetThemeColor(antiPortalButton.GetComponent<Button>().colors);
+            //antiPortalButton.transform.localPosition += new Vector3(-840f, -60f);
+            antiPortalButton.GetComponent<Button>().onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
+            antiPortalButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            antiPortalButton.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                Variables.antiPortal = !Variables.antiPortal;
+                antiPortalButton.GetComponentInChildren<Text>().text = !Variables.antiPortal ? "Enable\nPortals" : "Disable\nPortals";
+                antiPortalButton.GetComponentInChildren<Text>().color = !Variables.antiPortal ? Color.red : Color.green;
             });
         }
 
-        private void CustomMenu()
+        private void CreateFlyButton()
         {
-
+            GameObject original = customMenu.transform.Find("ClearRoomHub").gameObject;
+            flyButton = GameObject.Instantiate(original, original.transform.position, default(Quaternion), customMenu.transform);
+            flyButton.SetActive(true);
+            flyButton.name = "FlyButton";
+            flyButton.GetComponent<UiTooltip>().text = "Toggle Fly On/Off";
+            flyButton.GetComponentInChildren<Text>().text = !base.gameObject.GetComponent<Cheats>().isFlying ? "Enable\nFly" : "Disable\nFly";
+            flyButton.GetComponentInChildren<Text>().color = !base.gameObject.GetComponent<Cheats>().isFlying ? Color.green : Color.green;
+            flyButton.GetComponent<Button>().colors = Functions.SetThemeColor(flyButton.GetComponent<Button>().colors);
+            flyButton.GetComponent<Button>().onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
+            flyButton.GetComponent<Button>().onClick.RemoveAllListeners();
+            flyButton.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                base.gameObject.GetComponent<Cheats>().ToggleFly();
+                flyButton.GetComponentInChildren<Text>().text = !base.gameObject.GetComponent<Cheats>().isFlying ? "Enable\nFly" : "Disable\nFly";
+                flyButton.GetComponentInChildren<Text>().color = !base.gameObject.GetComponent<Cheats>().isFlying ? Color.green : Color.red;
+            });
+            flyButton.transform.localPosition += new Vector3(420f, 0f);
         }
     }
 }
