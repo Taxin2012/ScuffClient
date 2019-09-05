3722823cd1d12ec558702859466fcb56f9161df9
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2019-09-05 04:16:34 +0200

    cache world info here instead of running foreach in update loop (much more resource friendly) - empty class as of this update

---
+++ ScuffClient/Misc/WorldCache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRC;
+
+namespace ScuffClient.Misc
+{
+    public class WorldCache
+    {
+        private static List<Player> playerCache;
+    }
+}
